056db9ff40d7263b1939263877a2b687ef00037b
Luda Modna <ludmilamodna3@gmail.com>
2025-05-12 12:43:38 +0300

    Added props

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,59 +3,37 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 function App() {
+  const isLight = true;
+  const name = "Ivo";
   return (
     <>
-      <Header />
-      <div title="Hello">Hello React</div>
-      <UserInfo />
-      <News />
-      <Footer />
+      <ThemeButton isLightTheme={isLight} />
+      <Greeting isGreeting={true} userName={name} />
+      <Greeting isGreeting={false} userName="Max" />
     </>
   );
 }
 
-function Header() {
-  return (
-    <header className="header">
-      <h1>Page title</h1>
-    </header>
-  );
-}
+function ThemeButton(props) {
+  const { isLightTheme } = props;
 
-function UserInfo() {
-  const user = {
-    name: "Test",
-    age: 17,
-    imgSrc:
-      "https://i.pinimg.com/736x/b0/f1/72/b0f172fee313c97a7e5c7dc2a813a7a0.jpg",
+  const buttonStyle = {
+    color: isLightTheme ? "black" : "white",
+    backgroundColor: isLightTheme ? "white" : "black",
   };
 
-  const userNameStyle = { color: user.age >= 18 ? "blue" : "green" };
-
   return (
-    <article className="userCard">
-      <h2 style={userNameStyle}>{user.name}</h2>
-      <img src={user.imgSrc} alt={user.name} />
-    </article>
+    <button style={buttonStyle}>
+      Switch to {isLightTheme ? "dark" : "light"}
+    </button>
   );
 }
 
-function Footer() {
+function Greeting(props) {
+  const {isGreeting, userName } = props;
   return (
-    <footer>
-      <h2>Footer Page</h2>
-    </footer>
-  );
-}
-
-function News() {
-  const news = { title: "Some news", body: "Something happend", isGood: false };
-  const styleNew = { backgroundColor: news.isGood ? "green" : "red" };
-
-  return (
-    <article style={styleNew}>
-      <h2>{news.title}</h2>
-      <p>{news.body}</p>
-    </article>
+    <h2>
+      {isGreeting ? "Hello" : "Bye"}, {userName}!
+    </h2>
   );
 }
