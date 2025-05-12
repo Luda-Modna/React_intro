4ed50de51f4d3c52114896e68125adf4fd5b4eb8
Luda Modna <ludmilamodna3@gmail.com>
2025-05-12 14:02:09 +0300

    Added userCard React

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,37 +3,57 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
 
 function App() {
-  const isLight = true;
-  const name = "Ivo";
+  const user = {
+    imgSrc:
+      "https://i.pinimg.com/736x/3c/d3/76/3cd3764ff576208a8305732ef8156f0a.jpg",
+    name: "Anna",
+    surname: "Bell",
+    gender: "male",
+    followers: 10,
+  };
   return (
     <>
-      <ThemeButton isLightTheme={isLight} />
-      <Greeting isGreeting={true} userName={name} />
-      <Greeting isGreeting={false} userName="Max" />
+      <UserCard userInfo={user} />
     </>
   );
 }
 
-function ThemeButton(props) {
-  const { isLightTheme } = props;
+function UserCard(props) {
+  const { userInfo } = props;
 
-  const buttonStyle = {
-    color: isLightTheme ? "black" : "white",
-    backgroundColor: isLightTheme ? "white" : "black",
+  const [followers, setFollowers] = React.useState(userInfo.followers);
+  const addFollowersHandler = () => {
+    setFollowers(followers + 1);
   };
 
-  return (
-    <button style={buttonStyle}>
-      Switch to {isLightTheme ? "dark" : "light"}
-    </button>
-  );
-}
+  const [liked, setLiked] = React.useState(false);
+  const changeColorHeardHandler = () => {
+    setLiked(!liked);
+  };
+  const heardStyle = { color: liked ? "red" : "grey" ,cursor:'pointer',};
 
-function Greeting(props) {
-  const {isGreeting, userName } = props;
+  const styleCard = {
+    boxShadow: userInfo.gender === "male" ? "0 0 10px green" : "0 0 10px blue",
+  };
   return (
-    <h2>
-      {isGreeting ? "Hello" : "Bye"}, {userName}!
-    </h2>
+    <article className="userCard">
+      <img
+        style={styleCard}
+        src={userInfo.imgSrc}
+        alt={userInfo.name + " " + userInfo.surname}
+      />
+      <button onClick={addFollowersHandler} className="btnAddFollowers">
+        +
+      </button>
+      <h2>{userInfo.name + " " + userInfo.surname}</h2>
+      <p>Followers: {followers}</p>
+      <span>
+        <i
+          onClick={changeColorHeardHandler}
+          style={heardStyle}
+          className="fa-solid fa-heart"
+        ></i>
+      </span>
+    </article>
   );
 }
